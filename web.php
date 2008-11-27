d33b49b6c2e3e26031309116538354b5492c5163
Zach Ahn <me@zachahn.com>
2008-11-27 01:20:18 -0500

    views work

--- web.php
+++ web.php
@@ -1,6 +1,21 @@
 <?php
 define('SCRIPT', $_SERVER['SCRIPT_NAME']);
 define('REQUEST', $_SERVER['REQUEST_URI']);
+$v = array();
+
+$stat = array();
+$stat[200] = '200 OK';
+$stat[301] = '301 Moved Permanently';
+$stat[302] = '302 Found';
+$stat[304] = '304 Not Modified';
+$stat[307] = '307 Temporary Redirect';
+$stat[400] = '400 Bad Request';
+$stat[401] = '401 Authorization Required';
+$stat[403] = '403 Forbidden';
+$stat[404] = '404 Not Found';
+$stat[410] = '410 Gone';
+$stat[500] = '500 Internal Server Error';
+$stat[501] = '501 Method Not Implemented';
 
 $url = substr(REQUEST, strrpos(SCRIPT, '/'));
 while (substr($url, -1) == '/') $url = substr($url, 0, -1);
@@ -15,15 +30,28 @@ function URL($u) {
 function r($s, $b, $h) {
 	global $stat;
 	header('HTTP/1.1 '. $stat[$s]);
-	if (!empty($h)) {
+	if (!empty($h))
 		if (is_string($h)) header($h);
 		elseif (is_array($h)) foreach ($h as $i) header($i);
-	}
 	echo $b;
 }
 
 function redirect($l) {
 	r(301, '', 'Location: '. URL($l));
+}
+
+// Render takes the view to be run. $_l is used as layout if exists
+// Layouts must be in the `views/` directory.
+function render($_f, $_l = 'layout') {
+	global $v;
+	ob_start();
+	foreach ($v as $_k => $_v) $$_k = $_v;
+	include 'views/'. trim($_f) .'.php';
+	$yield = ob_get_clean();
+
+	if (is_string($_l))
+		if (file_exists($f = 'views/'. $_l)) include $f;
+	else echo $yield;
 }
 
 // Prepares string for url regex
