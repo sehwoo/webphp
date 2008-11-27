8176a18e1088c43c6f16023d59da28ef5db9ef39
Zach Ahn <me@zachahn.com>
2008-11-27 02:11:17 -0500

    removed constants, edited render(), _render(), and made some more minor changes

--- web.php
+++ web.php
@@ -1,7 +1,5 @@
 <?php
-define('SCRIPT', $_SERVER['SCRIPT_NAME']);
-define('REQUEST', $_SERVER['REQUEST_URI']);
-define('PORT', $_SERVER['SERVER_PORT']);
+$s = $_SERVER;
 $v = array();
 
 $stat = array();
@@ -18,14 +16,15 @@ $stat[410] = '410 Gone';
 $stat[500] = '500 Internal Server Error';
 $stat[501] = '501 Method Not Implemented';
 
-$url = substr(REQUEST, strrpos(SCRIPT, '/'));
+$url = substr($s['REQUEST_URI'], strrpos($s['SCRIPT_NAME'], '/'));
 while (substr($url, -1) == '/') $url = substr($url, 0, -1);
 if ($url == '') $url = '/';
 
 // $u can be either `/test` or `test`.
 function URL($u) {
 	while ($u[0] == '/') $u = substr($u, 1, strlen($u) - 1);
-	$u = substr(SCRIPT, 0, strrpos(SCRIPT, '/') + 1) . $u;
+	$u = substr($s['SCRIPT_NAME'], 0, 
+		strrpos($s['SCRIPT_NAME'], '/') + 1) . $u;
 	return 'http://'. $_SERVER['SERVER_NAME'].
 		((PORT == '80')? '':':'.PORT).$u;
 }
@@ -43,6 +42,13 @@ function redirect($l) {
 	r(301, '', 'Location: '. URL($l));
 }
 
+function _render($yield, $_l = 'layout') {
+	if (is_string($_l))
+		if (file_exists($f = 'views/'. $_l)) include $f;
+		elseif (file_exists($f = 'views/'. $_l .'.php')) include $f;
+	else echo $yield;
+}
+
 // Render takes the view to be run. $_l is used as layout if exists
 // Layouts must be in the `views/` directory.
 function render($_f, $_l = 'layout') {
@@ -50,11 +56,7 @@ function render($_f, $_l = 'layout') {
 	ob_start();
 	foreach ($v as $_k => $_v) $$_k = $_v;
 	include 'views/'. trim($_f) .'.php';
-	$yield = ob_get_clean();
-
-	if (is_string($_l))
-		if (file_exists($f = 'views/'. $_l)) include $f;
-	else echo $yield;
+	_render(ob_get_clean(), $_l);
 }
 
 // Prepares string for url regex
@@ -77,17 +79,15 @@ function run($urls) {
 	global $url;
 
 	foreach ($urls as $r => $c) {
-		// $r, $c = route, controller
-		$r = prep_reg($r);
-		preg_match($r, $url, $m);
-		// $m = matches
+		// $r, $c, $m = route, controller, matches
+		preg_match($r = prep_reg($r), $url, $m);
 
 		if (count($m) > 0) {
 			// $m includes the url that it matches, we dislike
 			array_shift($m);
 			foreach ($m as &$i) $i = '"'. $i .'"';
 			serve($c, (count($_POST) == 0)? 'get':'post', $m);
-			return '';
+			return;
 		} // else it isn't a match so go to the next item in array
 	}
 	// If the code reaches this point, there was no match
